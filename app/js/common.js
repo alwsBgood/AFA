c8f2aef4260412fc05849a6f61532bf1a4238040
Dmitriy <DmitryChebotaru@GoITAdmins-iMac-7.local>
2017-03-24 16:24:03 +0200

    six screens

--- app/js/common.js
+++ app/js/common.js
@@ -142,22 +142,22 @@ $(window).scroll(function() {
 
 //YOUTUBE
 
-$(function() {
-  $(".youtube").each(function() {
-    $(this).css('background-image', 'url(http://i.ytimg.com/vi/' + this.id + '/sddefault.jpg)');
+// $(function() {
+//   $(".youtube").each(function() {
+//     $(this).css('background-image', 'url(http://i.ytimg.com/vi/' + this.id + '/sddefault.jpg)');
 
-    $(this).append($('<div/>', {'class': 'play'}));
+//     $(this).append($('<div/>', {'class': 'play'}));
 
-    $(document).delegate('#'+this.id, 'click', function() {
-      var iframe_url = "https://www.youtube.com/embed/" + this.id + "?autoplay=1&autohide=1";
-      if ($(this).data('params')) iframe_url+='&'+$(this).data('params');
+//     $(document).delegate('#'+this.id, 'click', function() {
+//       var iframe_url = "https://www.youtube.com/embed/" + this.id + "?autoplay=1&autohide=1";
+//       if ($(this).data('params')) iframe_url+='&'+$(this).data('params');
 
-      var iframe = $('<iframe/>', {'frameborder': '0', 'src': iframe_url, 'width': $(this).width(), 'height': $(this).height() })
+//       var iframe = $('<iframe/>', {'frameborder': '0', 'src': iframe_url, 'width': $(this).width(), 'height': $(this).height() })
 
-      $(this).replaceWith(iframe);
-    });
-  });
-});
+//       $(this).replaceWith(iframe);
+//     });
+//   });
+// });
 
 //  UP BUTTON
 
