2a1598e86891ea9f50e93b6345d2602fd54860f7
Dmitriy <dima.chebotaru@gmail.com>
2017-03-27 08:01:02 +0300

    Desctop final

--- app/js/common.js
+++ app/js/common.js
@@ -1,9 +1,6 @@
-if (localStorage.name && localStorage.email && localStorage.phone)  {
-  // запись сохраненных данных сразу в поля, если надо
-  $('input[name="name"]').val(localStorage.name);
-  $('input[type="email"]').val(localStorage.email);
-  $('input[type="tel"]').val(localStorage.phone);
-}
+if (localStorage.email){$('input[type="email"]').val(localStorage.email);}
+if (localStorage.name){$('input[name="name"]').val(localStorage.name);}
+if (localStorage.phone){$('input[type="tel"]').val(localStorage.phone);}
 
 $(function() {
   $("[name=send]").click(function (e) {
@@ -82,25 +79,26 @@ $(function() {
           $("[name=send]").removeAttr("disabled");
         }, 1000);
         $('div.md-show').removeClass('md-show');
-        dataLayer.push({
-          'form_type': formType,
-          'event': "form_submit"
-        });
+        $("#call_ok")[0].click()
+        // dataLayer.push({
+        //   'form_type': formType,
+        //   'event': "form_submit"
+        // });
           // Отправка в базу данных
-          $.ajax({
-           type: 'POST',
-           url: 'db/registration.php',
-           dataType: 'json',
-           data: form.serialize(),
-           success: function(response) {
-             console.info(response);
-             console.log(form.serialize());
-             if (response.status == 'success') {
-              $('form').trigger("reset");
-              window.location.href = '/success';
-            }
-          }
-        });
+        //   $.ajax({
+        //    type: 'POST',
+        //    url: 'db/registration.php',
+        //    dataType: 'json',
+        //    data: form.serialize(),
+        //    success: function(response) {
+        //      console.info(response);
+        //      console.log(form.serialize());
+        //      if (response.status == 'success') {
+        //       $('form').trigger("reset");
+        //       // window.location.href = '/success';
+        //     }
+        //   }
+        // });
       },
       error: function(xhr, str) {
         console.log("Erorr")
@@ -127,6 +125,64 @@ jQuery(function($){
  $("input[type='tel']").mask("+9 (999) 999-9999");
 });
 
+
+$(document).ready(function(){
+ $('.slider_testimonials').slick({
+    slidesToShow: 1,
+    dots: false,
+    arrows: false,
+    slidesToScroll: 1,
+    asNavFor: '.slider_control'
+  });
+});
+
+$(document).ready(function(){
+ $('.slider_control').slick({
+    slidesToShow: 1,
+    dots: false,
+    arrows: true,
+    fade: true,
+    slidesToScroll: 1,
+    asNavFor: '.slider_testimonials'
+  });
+});
+
+
+// INPUT STYLIZATION
+
+(function() {
+    // trim polyfill : https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/String/Trim
+    if (!String.prototype.trim) {
+        (function() {
+            // Make sure we trim BOM and NBSP
+            var rtrim = /^[\s\uFEFF\xA0]+|[\s\uFEFF\xA0]+$/g;
+            String.prototype.trim = function() {
+                return this.replace(rtrim, '');
+            };
+        })();
+    }
+
+    [].slice.call( document.querySelectorAll( 'input.input__field' ) ).forEach( function( inputEl ) {
+        // in case the input is already filled..
+        if( inputEl.value.trim() !== '' ) {
+            classie.add( inputEl.parentNode, 'input--filled' );
+        }
+
+        // events:
+        inputEl.addEventListener( 'focus', onInputFocus );
+        inputEl.addEventListener( 'blur', onInputBlur );
+    } );
+
+    function onInputFocus( ev ) {
+        classie.add( ev.target.parentNode, 'input--filled' );
+    }
+
+    function onInputBlur( ev ) {
+        if( ev.target.value.trim() === '' || ev.target.value.trim() == '(___) ___-____' ) {
+            classie.remove( ev.target.parentNode, 'input--filled' );
+        }
+    }
+})();
 
 
 // Scroll BAR
@@ -196,47 +252,188 @@ $('.md-overlay').click(function() {
 });
 
 
+//animate
+    var decimal_places = 1;
+    var decimal_factor = decimal_places === 0 ? 1 : decimal_places * 1;
+
+// waypoints
+$('#sec_06').waypoint(
+    function() {
+        if ($("#sec_06").hasClass("animated")) {
+        }
+        else{
+            $("#sec_06").addClass("animated");
+        }
+    },
+    {offset: "150px"}
+
+);
+
+$('#sec_09').waypoint(
+    function() {
+        if ($("#sec_09").hasClass("animated")) {
+        }
+        else{
+            $("#sec_09").addClass("animated");
+        }
+    },
+    {offset: "250px"}
+);
+
+$('#sec_11').waypoint(
+    function() {
+        if ($("#sec_11").hasClass("number")) {
+        }
+        else{
+            $("#sec_11").addClass("number");
+            $('.first_animate_number').animateNumber(
+            {
+                number: 600 * decimal_factor,
+
+                numberStep: function(now, tween) {
+                    var floored_number = Math.floor(now) / decimal_factor,
+                    target = $(tween.elem);
+                    if (decimal_places > 0) {
+                        floored_number = floored_number.toFixed();
+                    }
+
+                    target.text(floored_number + ' +');
+                }
+            },
+            2000
+            );
+
+            $('.second_animate_number').animateNumber(
+            {
+                number: 2670000 * decimal_factor,
+
+                numberStep: function(now, tween) {
+                    var floored_number = Math.floor(now) / decimal_factor,
+                    target = $(tween.elem);
+                    if (decimal_places > 0) {
+                        floored_number = floored_number.toFixed();
+                    }
+
+                    target.text('$ ' + floored_number + ' +');
+                }
+            },
+            2000
+            );
+
+            $('.third_animate_number').animateNumber(
+            {
+                number: 10500 * decimal_factor,
+
+                numberStep: function(now, tween) {
+                    var floored_number = Math.floor(now) / decimal_factor,
+                    target = $(tween.elem);
+                    if (decimal_places > 0) {
+                        floored_number = floored_number.toFixed();
+                    }
+
+                    target.text('$ ' + floored_number);
+                }
+            },
+            2000
+            );
+
+            $('.fourth_animate_number').animateNumber(
+            {
+                number: 60 * decimal_factor,
+
+                numberStep: function(now, tween) {
+                    var floored_number = Math.floor(now) / decimal_factor,
+                    target = $(tween.elem);
+                    if (decimal_places > 0) {
+                        floored_number = floored_number.toFixed();
+                    }
+
+                    target.text(floored_number + ' +');
+                }
+            },
+            2000
+            );
+        }
+    },
+    {offset: "550px"}
+
+);
+
+
+
+// ====== Google Maps =====
+
+function init() {
+
+    var mapOptions = {
+        zoom: 16,
+        center: new google.maps.LatLng(50.437580, 30.527281),
+        mapTypeControl: false,
+        scrollwheel: false,
+        streetViewControl: false,
+        styles: [{"featureType":"water","elementType":"geometry","stylers":[{"color":"#e9e9e9"},{"lightness":17}]},{"featureType":"landscape","elementType":"geometry","stylers":[{"color":"#f5f5f5"},{"lightness":20}]},{"featureType":"road.highway","elementType":"geometry.fill","stylers":[{"color":"#ffffff"},{"lightness":17}]},{"featureType":"road.highway","elementType":"geometry.stroke","stylers":[{"color":"#ffffff"},{"lightness":29},{"weight":0.2}]},{"featureType":"road.arterial","elementType":"geometry","stylers":[{"color":"#ffffff"},{"lightness":18}]},{"featureType":"road.local","elementType":"geometry","stylers":[{"color":"#ffffff"},{"lightness":16}]},{"featureType":"poi","elementType":"geometry","stylers":[{"color":"#f5f5f5"},{"lightness":21}]},{"featureType":"poi.park","elementType":"geometry","stylers":[{"color":"#dedede"},{"lightness":21}]},{"elementType":"labels.text.stroke","stylers":[{"visibility":"on"},{"color":"#ffffff"},{"lightness":16}]},{"elementType":"labels.text.fill","stylers":[{"saturation":36},{"color":"#333333"},{"lightness":40}]},{"elementType":"labels.icon","stylers":[{"visibility":"off"}]},{"featureType":"transit","elementType":"geometry","stylers":[{"color":"#f2f2f2"},{"lightness":19}]},{"featureType":"administrative","elementType":"geometry.fill","stylers":[{"color":"#fefefe"},{"lightness":20}]},{"featureType":"administrative","elementType":"geometry.stroke","stylers":[{"color":"#fefefe"},{"lightness":17},{"weight":1.2}]}]
+      };
+
+    var mapElement = document.getElementById('map');
+
+    var map = new google.maps.Map(mapElement, mapOptions);
+
+    var markerImage = new google.maps.MarkerImage('img/mapmarker.png');
+
+    var marker = new google.maps.Marker({
+      position: new google.maps.LatLng(50.437580, 30.527281),
+      map: map,
+      title: 'Snazzy!',
+      icon: markerImage
+    });
+  }
+
+  $(document).ready(init);
+
+
+
+
 // Perfect Pxel
 
-$('body').each(function() {
-  var body = $(this);
-  var img_url = $(this).data('img');
-  var img = new Image();
-  img.src = img_url;
-  img.onload = function(){
-    var ppbox = '<div id="pp" style="background: url('+img_url+') no-repeat 50% 0%;top:0;width:100%;position:absolute;z-index:1000000;opacity:0.5;height:'+img.height+'px"></div>';
-    var ppbtn = '<button onclick="myOff()" id="ppbtn" style="position:fixed;top:0;right:0;z-index:1000001">ON</button>'
-    body.append(ppbox);
-    body.append(ppbtn);
-  };
-});
-function myOff() {
-  var ppbtntext = $('#ppbtn').text();
-  if (ppbtntext == 'ON') {
-    $('#ppbtn').text('OFF');
-    $('#pp').css('display', 'none');
-  } else {
-    $('#ppbtn').text('ON');
-    $('#pp')        .css({
-      ' z-index' : '1000000',
-      display: 'block'
-    });
+// $('body').each(function() {
+//   var body = $(this);
+//   var img_url = $(this).data('img');
+//   var img = new Image();
+//   img.src = img_url;
+//   img.onload = function(){
+//     var ppbox = '<div id="pp" style="background: url('+img_url+') no-repeat 50% 0%;top:0;width:100%;position:absolute;z-index:1000000;opacity:0.5;height:'+img.height+'px"></div>';
+//     var ppbtn = '<button onclick="myOff()" id="ppbtn" style="position:fixed;top:0;right:0;z-index:1000001">ON</button>'
+//     body.append(ppbox);
+//     body.append(ppbtn);
+//   };
+// });
+// function myOff() {
+//   var ppbtntext = $('#ppbtn').text();
+//   if (ppbtntext == 'ON') {
+//     $('#ppbtn').text('OFF');
+//     $('#pp').css('display', 'none');
+//   } else {
+//     $('#ppbtn').text('ON');
+//     $('#pp')        .css({
+//       ' z-index' : '1000000',
+//       display: 'block'
+//     });
 
-  }
-}
+//   }
+// }
 
-$('html').keydown(function(){
-  var ppbtntext = $('#ppbtn').text();
-  if (event.keyCode == 81) {
-    if (ppbtntext == 'ON') {
-      $('#ppbtn').text('OFF');
-      $('#pp').css('display', 'none');
-    } else {
-      $('#ppbtn').text('ON');
-      $('#pp')        .css({
-        ' z-index' : '1000000',
-        display: 'block'
-      });
-    }
-  }
-});
+// $('html').keydown(function(){
+//   var ppbtntext = $('#ppbtn').text();
+//   if (event.keyCode == 81) {
+//     if (ppbtntext == 'ON') {
+//       $('#ppbtn').text('OFF');
+//       $('#pp').css('display', 'none');
+//     } else {
+//       $('#ppbtn').text('ON');
+//       $('#pp')        .css({
+//         ' z-index' : '1000000',
+//         display: 'block'
+//       });
+//     }
+//   }
+// });
